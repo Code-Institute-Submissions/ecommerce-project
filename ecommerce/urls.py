9f4176052bae2c026046340bb19c632f56e4cb47
Noelle Browne <noelle.browne@yahoo.ie>
2019-04-12 21:23:49 +0000

    added user profile to urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,11 +7,13 @@ from bugs import urls as urls_bugs
 from cart import urls as urls_cart
 from search import urls as urls_search
 from checkout import urls as urls_checkout
+from cart.views import view_cart, add_to_cart, adjust_cart
 from bugs.views import all_bugs
 from home.views import all_index
-from accounts.views import index
+from accounts.views import index,  logout, login, register, user_profile
 from django.views import static
 from .settings import MEDIA_ROOT
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,4 +25,8 @@ urlpatterns = [
     url(r'^bugs/', include(urls_bugs)),
     url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
+    url(r'^accounts/profile/$', user_profile, name="profile"),
+    url(r'^accounts/logout/$', logout, name="logout"),
+    url(r'^accounts/login/$', login, name="login"),
+    url(r'^accounts/register/$', register, name="register"),
 ]
