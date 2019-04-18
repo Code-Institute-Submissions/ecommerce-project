017ec67ccea74cdd6ccba48126a53ddb12fc0979
Noelle Browne <noelle.browne@yahoo.ie>
2019-04-18 23:00:09 +0000

    added bugs to urls.py

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from .views import all_bugs
+from .views import all_bugs, add_bug, one_bug, comment
 
 
 urlpatterns = [
     url(r'^$', all_bugs, name='bugs'),
+    url(r'^add/$', add_bug, name='add_bugs'),
+    url(r'^comment/(?P<bug_id>.*)$', comment, name='comment_bug'),
+    url(r'^(?P<bug_id>.*)$', one_bug, name='one_bugs'),
     ]
