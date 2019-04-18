27e7a97f67ff16ba8fcd2cf5e74a094362e74f68
Noelle Browne <noelle.browne@yahoo.ie>
2019-04-18 23:00:33 +0000

    added bugs to models.py

--- bugs/models.py
+++ bugs/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Bugs(models.Model):
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-    
+    status = models.CharField(max_length=254, default='Todo')  # Todo, Pending, Completed
+
     def __str__(self):
         return self.name
+
+
+class Comments(models.Model):
+    bug_id = models.ForeignKey(Bugs, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    comment = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.bug_id
