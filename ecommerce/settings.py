b080f3829a9ec2402a6c9151be6ccab6c8702715
Noelle Browne <noelle.browne@yahoo.ie>
2019-04-25 09:49:54 +0000

    changed for heroku deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import env
 import dj_database_url
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -170,3 +170,5 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+django_heroku.settings(locals())
